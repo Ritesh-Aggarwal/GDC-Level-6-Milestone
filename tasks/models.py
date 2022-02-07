f081b26fde6aeb3f79ed21f60ae6cc31ad295726
Ritesh <riteshaggarwal2001@gmail.com>
2022-02-07 14:38:54 +0530

    styled forms and templates using tailwind

--- tasks/models.py
+++ tasks/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
@@ -10,6 +11,16 @@ class Task(models.Model):
     created_date = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
     user = models.ForeignKey(User , on_delete=models.CASCADE , null=True,blank=True)
+    priority = models.IntegerField(default=-1)
+
+    def pretty_date(self):
+        return self.created_date.strftime("%a %d %b")
+
+    def calculateComplete(self):
+        return Task.objects.filter(deleted=False, completed=True,user=self.user).count()
+    
+    def calculateTotal(self):
+        return Task.objects.filter(deleted=False,user=self.user).count()
 
     def __str__(self):
         return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,58 +1,155 @@
 # Add your Views Here
-from django.views.generic.list import ListView
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.db.models import Avg, Max, Min
+from django.forms import ModelForm, ValidationError
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
 
 from tasks.models import Task
 
 
-class GenericListView(ListView):
-    model = Task
-    template_name = "index.html"
-    context_object_name = "tasks"
-    paginate_by = 5
-
+class AuthorizeLoginUser(LoginRequiredMixin):
     def get_queryset(self):
-        search_term = self.request.GET.get("search")
-        tasks = Task.objects.filter(deleted=False, completed=False)
-        if search_term:
-            tasks = tasks.filter(title__icontains=search_term)
+        tasks = Task.objects.filter(deleted=False,user=self.request.user) 
         return tasks
 
 
-def index(request):
-    existing_tasks = Task.objects.filter(deleted=False, completed=False)
-    return render(request, "index.html", {"tasks": existing_tasks})
+class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = ' bg-gray-100 rounded-lg p-2'
 
 
-def completed_task_view(request):
-    completed_tasks = Task.objects.filter(deleted=False, completed=True)
-    return render(request, "completed.html", {"tasks": completed_tasks})
+#signup
+class UserCreateView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = "user_create.html"
+    success_url = "/user/login"
+
+class CustomLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = ' bg-gray-100 rounded-lg p-2'
+
+#login
+class UserLoginView(LoginView,CustomLoginForm):
+    form_class = CustomLoginForm
+    template_name = "user_login.html"
+
+#modelform
+class TaskCreateForm(ModelForm):
+    
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+        if(len(title) < 10):
+            raise ValidationError("Title is too short")
+        return title.upper()
+    
+    def clean_priority(self):
+        p = self.cleaned_data['priority']
+        if(p < 1 or p > 10):
+            raise ValidationError("Priority should be between 1 and 10")
+        return p
+    
+    class Meta:
+        model = Task
+        fields = ['title','description','priority','completed']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = ' bg-gray-100 rounded-lg p-2'
+        self.fields['description'].widget.attrs['class'] += ' h-44'
+        self.fields['completed'].widget.attrs['class'] = 'h-4 w-4 rounded-sm cursor-pointer'
+
+#create view
+class GenericTaskCreateView(AuthorizeLoginUser,CreateView):
+    model = Task
+    form_class = TaskCreateForm
+    template_name = "task_create.html"
+    success_url = "/tasks"
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.user = self.request.user
+        self.object.save()
+        parseDB = Task.objects.filter(deleted=False,completed=False,user=self.request.user,priority__gte=self.object.priority).order_by('priority','-created_date')
+        exisitng_priorities = [self.object.priority]
+        for t in parseDB[1:]:
+            if t.priority in exisitng_priorities:
+                t.priority = t.priority+1;
+                exisitng_priorities.append(t.priority)
+                t.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+#deleteview
+class GenericTaskDeleteView(AuthorizeLoginUser,DeleteView):
+    model = Task
+    template_name = "task_delete.html"
+    success_url = '/tasks'
+
+#updateview
+class GenericTaskUpdateView(AuthorizeLoginUser,UpdateView):
+    model = Task
+    form_class = TaskCreateForm
+    template_name = "task_update.html"
+    success_url = "/tasks"
+
+#detailview
+class GenericTaskDetailView(AuthorizeLoginUser,DetailView):
+    model = Task
+    template_name = "task_detail.html"
+
+class GenericListView(LoginRequiredMixin,ListView):
+    model = Task
+    template_name = "index.html"
+    context_object_name = "tasks"
+    paginate_by = 3
+
+    def get_queryset(self):
+        search_term = self.request.GET.get("search")
+        tasks = Task.objects.filter(deleted=False, completed=False,user=self.request.user).order_by('priority','-created_date')
+        if search_term:
+            tasks = tasks.filter(title__icontains=search_term).order_by('priority','-created_date')
+        return tasks
+
+class GenericCompleteTaskView(LoginRequiredMixin,ListView):
+    model = Task
+    template_name = "completed.html"
+    context_object_name = "tasks"
+    paginate_by = 4
+
+    def get_queryset(self):
+        tasks = Task.objects.filter(deleted=False,completed=True,user=self.request.user) 
+        return tasks
+
+class GenericAllTaskView(AuthorizeLoginUser,ListView):
+    model = Task
+    template_name = "all.html"
+    context_object_name = "tasks"
+    paginate_by = 3
 
 
-def all_tasks_view(request):
-    existing_tasks = Task.objects.filter(deleted=False, completed=False)
-    completed_tasks = Task.objects.filter(deleted=False, completed=True)
-    return render(
-        request,
-        "all.html",
-        {"tasks": {"completed": completed_tasks, "existing": existing_tasks}},
-    )
+# def complete_task_view(request, index):
+#     task = Task.objects.filter(id=index).update(completed=True)
+#     return HttpResponseRedirect("/tasks/")
 
+class GenericCompleteView(AuthorizeLoginUser,View):
+    model = Task
+    
+    def get(self,request):
+        return HttpResponseRedirect("/tasks/")
 
-def add_task_view(request):
-    task = Task(title=request.GET.get("task"))
-    task.save()
-    return HttpResponseRedirect("/tasks/")
+    def post(self,request):
+        task_id = request.POST.get('task_id')
+        task = Task.objects.filter(pk=task_id).update(completed=True)
+        return HttpResponseRedirect("/tasks/")
 
-
-def delete_task_view(request, index):
-    # origin = request.headers.get("Referer").split("/")[-2]
-    task = Task.objects.filter(id=index).update(deleted=True)
-    return HttpResponseRedirect("/tasks/")
-    # return HttpResponseRedirect(f"/{origin}/")
-
-
-def complete_task_view(request, index):
-    task = Task.objects.filter(id=index).update(completed=True)
-    return HttpResponseRedirect("/tasks/")
