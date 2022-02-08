80dbb7c2cac20f7e2e3f3e307c863a39eeb4fa9c
Ritesh <riteshaggarwal2001@gmail.com>
2022-02-08 14:29:23 +0530

    use exclude,bulk_update and select_for_update methods

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,9 @@
-# Add your Views Here
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
-from django.db.models import Avg, Max, Min
+from django.db import transaction
 from django.forms import ModelForm, ValidationError
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.views import View
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
@@ -14,12 +12,13 @@ from django.views.generic.list import ListView
 from tasks.models import Task
 
 
+# custom class to allow only logged in users and define the queryest accordingly
 class AuthorizeLoginUser(LoginRequiredMixin):
     def get_queryset(self):
         tasks = Task.objects.filter(deleted=False,user=self.request.user) 
         return tasks
 
-
+# custom signup form class inheriting from UserCreationForm and add css classes to field using constructor
 class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -27,24 +26,26 @@ class CustomUserCreationForm(UserCreationForm):
             self.fields[field].widget.attrs['class'] = ' bg-gray-100 rounded-lg p-2'
 
 
-#signup
-class UserCreateView(CreateView):
-    form_class = CustomUserCreationForm
-    template_name = "user_create.html"
-    success_url = "/user/login"
-
+# custom login form class inheriting from AuthenticationForm and add css classes to field using constructor
 class CustomLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = ' bg-gray-100 rounded-lg p-2'
 
-#login
+#signup view
+class UserCreateView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = "user_create.html"
+    success_url = "/user/login"
+
+
+#login view
 class UserLoginView(LoginView,CustomLoginForm):
     form_class = CustomLoginForm
     template_name = "user_login.html"
 
-#modelform
+#form for create/update task
 class TaskCreateForm(ModelForm):
     
     def clean_title(self):
@@ -56,9 +57,23 @@ class TaskCreateForm(ModelForm):
     def clean_priority(self):
         p = self.cleaned_data['priority']
         if(p < 1 or p > 10):
-            raise ValidationError("Priority should be between 1 and 10")
+            raise ValidationError("Priority should be between 1 and 10")      
         return p
-    
+
+    # NOTE: Logic for cascading same priority
+    # UPDATES:using select_for_update to lock rows to avoid concurrency issue
+            # using bulk_update to reduce queries
+            # using exclude to avoid using slice operation ([1:]) by using pk of current task 
+    def cascadePriority(self,user,priority,pk):
+        exisitng_priorities = [priority]
+        parseDB = Task.objects.select_for_update().filter(deleted=False,completed=False,user=user,priority__gte=priority).exclude(pk=pk).order_by('priority','-created_date')
+        with transaction.atomic():
+            for i in range(len(parseDB)):
+                if parseDB[i].priority in exisitng_priorities:
+                    parseDB[i].priority += 1;
+                    exisitng_priorities.append(parseDB[i].priority)
+        n = Task.objects.bulk_update(parseDB,['priority'])             
+
     class Meta:
         model = Task
         fields = ['title','description','priority','completed']
@@ -70,7 +85,8 @@ class TaskCreateForm(ModelForm):
         self.fields['description'].widget.attrs['class'] += ' h-44'
         self.fields['completed'].widget.attrs['class'] = 'h-4 w-4 rounded-sm cursor-pointer'
 
-#create view
+#create view to add a new task
+#NOTE : cascade logic moved to TaskCreateForm as a member function
 class GenericTaskCreateView(AuthorizeLoginUser,CreateView):
     model = Task
     form_class = TaskCreateForm
@@ -81,33 +97,42 @@ class GenericTaskCreateView(AuthorizeLoginUser,CreateView):
         self.object = form.save()
         self.object.user = self.request.user
         self.object.save()
-        parseDB = Task.objects.filter(deleted=False,completed=False,user=self.request.user,priority__gte=self.object.priority).order_by('priority','-created_date')
-        exisitng_priorities = [self.object.priority]
-        for t in parseDB[1:]:
-            if t.priority in exisitng_priorities:
-                t.priority = t.priority+1;
-                exisitng_priorities.append(t.priority)
-                t.save()
+        user = self.request.user
+        priority = self.object.priority
+        pk = self.object.id
+        form.cascadePriority(user,priority,pk)        
         return HttpResponseRedirect(self.get_success_url())
 
-#deleteview
+#Delete view
 class GenericTaskDeleteView(AuthorizeLoginUser,DeleteView):
     model = Task
     template_name = "task_delete.html"
     success_url = '/tasks'
 
-#updateview
+#update view of a task 
+#NOTE :  updated code to handle cascade priority
 class GenericTaskUpdateView(AuthorizeLoginUser,UpdateView):
     model = Task
     form_class = TaskCreateForm
     template_name = "task_update.html"
     success_url = "/tasks"
 
-#detailview
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.user = self.request.user
+        self.object.save()
+        user = self.request.user
+        priority = self.object.priority
+        pk = self.object.id
+        form.cascadePriority(user,priority,pk)                
+        return HttpResponseRedirect(self.get_success_url())
+
+#Detail view of a task
 class GenericTaskDetailView(AuthorizeLoginUser,DetailView):
     model = Task
     template_name = "task_detail.html"
 
+# View for viewing pending tasks
 class GenericListView(LoginRequiredMixin,ListView):
     model = Task
     template_name = "index.html"
@@ -121,6 +146,15 @@ class GenericListView(LoginRequiredMixin,ListView):
             tasks = tasks.filter(title__icontains=search_term).order_by('priority','-created_date')
         return tasks
 
+
+# View for viewing all task
+class GenericAllTaskView(AuthorizeLoginUser,ListView):
+    model = Task
+    template_name = "all.html"
+    context_object_name = "tasks"
+    paginate_by = 3
+
+# View for viewing completed task
 class GenericCompleteTaskView(LoginRequiredMixin,ListView):
     model = Task
     template_name = "completed.html"
@@ -131,17 +165,9 @@ class GenericCompleteTaskView(LoginRequiredMixin,ListView):
         tasks = Task.objects.filter(deleted=False,completed=True,user=self.request.user) 
         return tasks
 
-class GenericAllTaskView(AuthorizeLoginUser,ListView):
-    model = Task
-    template_name = "all.html"
-    context_object_name = "tasks"
-    paginate_by = 3
-
-
-# def complete_task_view(request, index):
-#     task = Task.objects.filter(id=index).update(completed=True)
-#     return HttpResponseRedirect("/tasks/")
-
+# completed_task route to mark task as completed 
+# task_id is a hidden input in the form with value of object.id
+# route is csrf protected and also requires user login
 class GenericCompleteView(AuthorizeLoginUser,View):
     model = Task
     
