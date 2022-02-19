517236011ef7687e04c5945da8f333e3221b7e1e
Ritesh-Aggarwal <riteshaggarwal2001@gmail.com>
2022-02-19 21:37:54 +0530

    create History model

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,6 @@
 
-from django.db import models
-
 from django.contrib.auth.models import User
+from django.db import models
 
 STATUS_CHOICES = (
     ("PENDING", "PENDING"),
@@ -10,15 +9,39 @@ STATUS_CHOICES = (
     ("CANCELLED", "CANCELLED"),
 )
 
+
+
+class History(models.Model):
+    task = models.ForeignKey("Task", on_delete=models.CASCADE)
+    old_status =  models.CharField(max_length=100, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+    new_status =  models.CharField(max_length=100, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+    updated_at =  models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.updated_at.strftime("%a %d %b")
+ 
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     completed = models.BooleanField(default=False)
-    created_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
     deleted = models.BooleanField(default=False)
     user = models.ForeignKey(User , on_delete=models.CASCADE , null=True,blank=True)
     priority = models.IntegerField(default=0)
     status = models.CharField(max_length=100, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+
+    def get_history(self):
+        return History.objects.filter(task=self)
+
+    def save(self, *args, **kwargs):
+        super().save( *args, **kwargs)
+        history = self.get_history()
+        if history.exists():
+            old_status = History.objects.filter(task=self).order_by('-updated_at').first().new_status
+            if old_status != self.status:
+                History.objects.create(task=self, old_status=old_status, new_status=self.status)
+        else:
+            History.objects.create(task=self, old_status='', new_status=self.status)
 
     def pretty_date(self):
         return self.created_date.strftime("%a %d %b")
