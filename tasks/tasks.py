0de7b7e3e16058650aab6bddf0b466b84a7733e7
Ritesh-Aggarwal <riteshaggarwal2001@gmail.com>
2022-02-28 23:21:06 +0530

    Update tasks.py

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -9,18 +9,29 @@ from django.db.models import Count
 from tasks.models import ReportSchedule, Task
 
 
-@periodic_task(run_every=timedelta(seconds=30))
+@periodic_task(run_every=timedelta(seconds=60))
 def send_scheduled_emails():
     print("send scheduled emails")
-    reports = ReportSchedule.objects.filter(report_at__contains=datetime.now().strftime("%H:%M")).exclude(email='')
+    update_qs = []
+    dt = datetime.now()
+    # filter all reports which have to be sent at times less than equal to now and
+    # exclude those which have been processed today,
+    # hence the reports missed in 24hr will be reprocessed
+    reports = ReportSchedule.objects.filter(report_at__lte=dt).exclude(last_run_at__day=dt.day)
     for report in reports:
         print(report)
         pending_tasks = Task.objects.filter(user=report.user,completed=False,deleted=False).values('status').annotate(total=Count('status')).order_by('total')
         email_content = f"Hey {report.user}, here is your daily report:\n"
         for status in pending_tasks:
             email_content += f"Task {status['status']} : {status['total']}\n"
-        send_mail("Pending tasks from Tasks Manager",email_content,"eg@eg.com",{report.email})
+        if report.email:
+            send_mail("Pending tasks from Tasks Manager",email_content,"eg@eg.com",{report.email})
         print(f"Email sent to {report.user.id}")
+        report.last_run_at = datetime.now()
+        # report.next_run_at = report.report_at + timedelta(days=1)
+        update_qs.append(report)
+    # bulkupdate does not call save() hence last_modified feature of model can't be used.
+    updated_qs = ReportSchedule.objects.bulk_update(update_qs,['last_run_at'])
 
 @app.task
 def bg_jobs():
